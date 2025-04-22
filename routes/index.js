fa503aaa31c8286d675be8fb2d4301748696cb3f
katiii-git <katymartagon@gmail.com>
2025-04-22 02:10:04 -0600

    Primer commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const { Router } =require('express');
+const controllers =require('../controllers');
+
+const router= Router();
+
+router.get('/', (req, res) => res.send('Welcome'));
+
+router.post('/users', controllers.createUser);
+
+router.get('/users', controllers.getAllUsers);
+
+router.delete('/users/:id', controllers.deleteUser);
+
+router.put('/users/:id', controllers.updateUser);
+
+module.exports = router;
